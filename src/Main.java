384fe0f3ac92cd07537f456bfb30488cd653d542
AmrARaouf <amr.abdelraouf93@gmail.com>
2014-11-24 04:24:47 +0200

    adds helper functions for Clause Form function

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,55 @@
-
 public class Main {
+
+	static String unify(String str1, String str2) {
+		// TODO: Implement unification algorithm
+		return null;
+	}
+
+	static String clauseForm(String expression) {
+		return renameClauseVariables(toSetOfClause(
+				 flatten(toCNF(discardAQuantifiers(
+						skolemize(renameVariables(pushNotInwards
+								(elimEmplication(elimEquivalence(expression))))))))));
+	}
+
+	static String elimEquivalence(String expression) {
+		return null;
+	}
+
+	static String elimEmplication(String expression) {
+		return null;
+	}
+
+	static String pushNotInwards(String expression) {
+		return null;
+	}
+
+	static String renameVariables(String expression) {
+		return null;
+	}
+
+	static String skolemize(String expression) {
+		return null;
+	}
+
+	static String discardAQuantifiers(String expression) {
+		return null;
+	}
+
+	static String toCNF(String expression) {
+		return null;
+	}
+
+	static String flatten(String expression) {
+		return null;
+	}
+
+	static String toSetOfClause(String expression) {
+		return null;
+	}
+
+	static String renameClauseVariables(String expression) {
+		return null;
+	}
 
 }
