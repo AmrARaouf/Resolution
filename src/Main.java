7d5ecf5f59af29beeea598fd5b6b15e974815c36
Malatawy15 <malatawy15@gmail.com>
2014-11-26 13:34:12 +0200

    Move parse to Var

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
-import unification.*;
-import clauseForm.*;
+import unification.Unification;
+import function.Var;
 
 public class Main {
 	
 	public static void main(String[] args) {
 		Unification u = new Unification();
-		System.out.println(u.parse("P(x,g(x),g(f(a)))").toString());
-		System.out.println(u.parse("P(f(u),v,v)").toString());
-		System.out.println(u.parse("P(a,y,f(y))").toString());
-		System.out.println(u.parse("P(z,z,u)").toString());
-		System.out.println(u.parse("f(x,g(x),x)").toString());
-		System.out.println(u.parse("f(g(u),g(g(z)),z)").toString());
+		System.out.println(Var.parse("P(x,g(x),g(f(a)))").toString());
+		System.out.println(Var.parse("P(f(u),v,v)").toString());
+		System.out.println(Var.parse("P(a,y,f(y))").toString());
+		System.out.println(Var.parse("P(z,z,u)").toString());
+		System.out.println(Var.parse("f(x,g(x),x)").toString());
+		System.out.println(Var.parse("f(g(u),g(g(z)),z)").toString());
 		System.out.println("===================================================");
 		u.unify("P(x,g(x),g(f(a)))", "P(f(u),v,v)");
 		System.out.println("===================================================");
--- src/function/Var.java
+++ src/function/Var.java
@@ -2,4 +2,49 @@ package function;
 
 public abstract class Var {
 	public String name;
+	
+	public static Var parse(String exp) {
+		if (exp.equals("")) {
+			return null;
+		}
+		int index = 0;
+		// Extract the name
+		String name = "";
+		while (index < exp.length() && Character.isLetter(exp.charAt(index))) {
+			name += exp.charAt(index++);
+		}
+		// Check if the string is empty now
+		if (exp.length() - index == 0) {
+			// If that's the case then this is a literal
+			return new Literal(name);
+		}
+		// This is a function then
+		Function f = new Function(name);
+		parseFunctionParameters(f, exp.substring(index + 1, exp.length() - 1));
+		return f;
+	}
+
+	private static void parseFunctionParameters(Function f, String params) {
+		int openBrackets = 0, startIndex = 0;
+		for (int i = 0; i < params.length(); i++) {
+			switch (params.charAt(i)) {
+			case '(':
+				openBrackets++;
+				break;
+			case ')':
+				openBrackets--;
+				break;
+			case ',':
+				if (openBrackets == 0) {
+					f.addParameter(parse(params.substring(startIndex, i)));
+					startIndex = i + 1;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		f.addParameter(parse(params.substring(startIndex, params.length())));
+	}
+	
 }
